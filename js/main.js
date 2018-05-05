0b09e79c2dc676af7b7a3eab0184b553fd2c4cd7
yersultanzada <yersultan.zada@gmail.com>
2018-05-05 23:59:18 +0600

    .

--- js/main.js
+++ js/main.js
@@ -25,5 +25,13 @@ $(document).ready(function () {
     dropdownMenu.click(function () {
             dropdownMenu.removeClass('active');
             $(this).addClass('active')
-    })
+    });
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
 });
